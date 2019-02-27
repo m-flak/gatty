8cc1e3b2b2a82ff8650853f6f59d9421958d10d0
Matthew Kehrer <matt91.mek@gmail.com>
2019-02-26 20:07:01 -0600

    well it segfaults :'(

--- gatty/__main__.py
+++ gatty/__main__.py
@@ -1,6 +1,11 @@
 # coding=utf-8
 
+import re
 import sys
+
+from prompt_toolkit import PromptSession
+from google.assistant.library import Assistant
+
 import gatty
 import gatty.oauth
 
@@ -9,5 +14,18 @@ if gatty.oauth.check_for_credfiles() is not True:
 	print("ENSURE YOU HAVE BOTH CLIENT_SECRET_xxx.JSON AND CREDENTIALS.JSON")
 	sys.exit(1)
 
-print("Welcome to gatty {}!!!".format(gatty.__version__))
+special_exit = "exit - exits"
 
+print("Welcome to gatty {}!!!".format(gatty.__version__))
+print("The following commands are special:\n\t\t{}\n\t\t".format(special_exit))
+
+session = PromptSession(message="Say something: ")
+
+with Assistant(gatty.oauth.produce_credentials(), gatty.oauth.api_ids()[1]) as google:
+	something = ""
+	
+	while re.fullmatch("^exit$", something, re.M | re.I) is None:
+		something = session.prompt()
+		response = google.send_text_query(something)
+
+sys.exit(0)
--- gatty/oauth.py
+++ gatty/oauth.py
@@ -1,21 +1,30 @@
 # coding=utf-8
 import os
 import pkg_resources
+import google.oauth2.credentials as goocred
 
 #you'll need to change this based on your personal, own oauth shiznit
 CLIENT_ID = "713269907379-4crgrrvelpq9f44d3u4pmntkokr70h6k.apps.googleusercontent.com"
+
+#i'm sure this'll need to be different as well
+PROJECT_ID = "gatty-aff42"
+MODEL_ID = "gatty-aff42-laptop-eosr6x"
+INSTANCE_ID = "Laptop"
 
 # you'll need the secret file and the credentials.json
 def secret_file_name():
 	global CLIENT_ID
 	return "%s%s%s" % ("client_secret_", CLIENT_ID, ".json")
 
+def get_dirs() -> tuple:
+	basedir = pkg_resources.resource_filename("gatty", ".")
+	return basedir, os.path.join(basedir, "data", "oauth")
+
 # this requires setup
 # TODO: add the setup steps from the Assistant SDK tutorial into setup
 # TODO: add setup :^)
 def check_for_credfiles() -> bool:
-	basedir = pkg_resources.resource_filename("gatty", ".")
-	oadir = os.path.join(basedir, "data", "oauth")
+	basedir, oadir = get_dirs()
 	
 	try:
 		fd = os.open("%s/%s" % (oadir, secret_file_name()), os.O_RDONLY)
@@ -26,3 +35,16 @@ def check_for_credfiles() -> bool:
 		return False
 	
 	return True
+
+def api_ids() -> tuple:
+	global PROJECT_ID
+	global MODEL_ID
+	global INSTANCE_ID
+	return PROJECT_ID, MODEL_ID, INSTANCE_ID
+
+def produce_credentials():
+	json_creds = os.path.join(get_dirs()[1], "credentials.json")
+	
+	credentials = goocred.Credentials.from_authorized_user_file(json_creds)
+	
+	return credentials
